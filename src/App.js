108e5630e95f3b9b4773c29288f98e4933bbd3d1
Salkoazer <wilsonveterano@gmail.com>
2021-07-22 19:35:04 +0100

    basic skeleton of weather app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
+import Weather from './components/Weather';
+import { Dimmer, Loader } from 'semantic-ui-react';
 
-function App() {
+export default function App() {
+
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+        console.log("lat" + lat);
+        console.log("long" + long);
+      });
+
+      // eslint-disable-next-line no-undef
+      await fetch(`${process.env.REACT_APP_API_URL}/weather?lat=${lat}&lon=${long}&units=metric&appid=${process.env.REACT_APP_API_KEY}`)
+        .then(res => res.json())
+        .then(result => {
+          setData(result)
+          console.log(result);
+        });
+    }
+    fetchData();
+  }, [lat, long])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <div className="Title">Weather App</div>
+        
+        {(typeof data.main != 'undefined') ? (
+          <div>
+          <Weather weatherData={data} />
+          </div>
+        ) : (
+          <div>
+            <Dimmer active>
+              <Loader>Loading...</Loader>
+            </Dimmer>
+          </div>
+        )}
+        <div className="Author">Created by Wilson Veterano</div>
+      </div>
     </div>
   );
 }
 
-export default App;
